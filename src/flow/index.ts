3a4500141810d9e3c1fb2509a709e049a7829c05
ScampuzanoM <sebascm3450@gmail.com>
2024-07-23 12:25:58 -0500

    feat: :fire: Ajuste chatbot

--- src/flow/index.ts
+++ src/flow/index.ts
@@ -3,12 +3,14 @@ import { createFlow } from '@builderbot/bot';
 import { mayoristaFlow } from './mayorista.flow';
 
 import { welcomeFlow } from "./welcome.flow";
-
+import {defaulFlow} from "./default.flow"
+import {mediaFlow} from "./media.flow"
 import {blackListFlow} from './mute.flow'
 import { idleFlow } from '../idle-custom'
 import { detalFlow } from './detal.flow';
+import {voiceNoteFlow} from './voce.flow'
 import * as dotenv from 'dotenv';
 import { personalizadoFlow } from './personalizado.flow';
 dotenv.config();
 
-export const flow = createFlow([welcomeFlow, mayoristaFlow, idleFlow,blackListFlow,detalFlow,personalizadoFlow])
+export const flow = createFlow([welcomeFlow, mayoristaFlow, idleFlow,blackListFlow,detalFlow,personalizadoFlow,defaulFlow,mediaFlow,voiceNoteFlow])
